95dee81ee3107add53f5ddacd162aa7f0ec7a729
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-02 21:47:11 +0800

    Added Config

---
+++ config/attendance.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+	'timezone' => env('ATTENDANCE_TIMEZONE', 'Asia/Kuala_Lumpur'),
+	'models' => [
+		'user' => \App\User::class,
+		'attendance' => \CleaniqueCoders\Attendance\Models\Attendance::class,
+		'type' => \CleaniqueCoders\Attendance\Models\AttendanceType::class,
+	],
+	'drivers' => [
+		'web' => \CleaniqueCoders\Attendance\Adapters\WebAdapter::class,
+		'api' => \CleaniqueCoders\Attendance\Adapters\ApiAdapter::class,
+		'console' => \CleaniqueCoders\Attendance\Adapters\ConsoleAdapter::class,
+	],
+];
