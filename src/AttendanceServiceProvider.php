ff1184c6b7cb68bb1d27674a841ebe145dd4932d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-02 21:48:30 +0800

    Publish Config & Migrations. Load Commands

--- src/AttendanceServiceProvider.php
+++ src/AttendanceServiceProvider.php
@@ -11,6 +11,34 @@ class AttendanceServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /*
+         * Configuration
+         */
+        $this->publishes([
+            __DIR__ . '/../config/attendance.php' => config_path('attendance.php'),
+        ], 'attendance-config');
+
+        /*
+         * Database - Migrations, Factories and Seeders
+         */
+        if (! class_exists('CreateAttendancesTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_attendance_types_table.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_attendance_types_table.php'),
+                __DIR__ . '/../database/migrations/create_attendances_table.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_attendances_table.php'),
+            ], 'attendance-database');
+        }
+
+        /*
+         * Commands
+         */
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Console\Commands\InstallCommand::class,
+                Console\Commands\LogAttendanceCommand::class,
+                Console\Commands\MakeAttendanceAdapterCommand::class,
+                Console\Commands\SeedAttendanceTypesCommand::class,
+            ]);
+        }
     }
 
     /**
